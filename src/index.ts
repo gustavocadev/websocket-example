f983ecdb9b8f5ddf8a5a20b7abbad4d829bf1a48
gustavocadev <gustavocadev@gmail.com>
2024-06-23 17:30:35 -0500

    Add web socket implementation

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,44 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
+import { cors } from '@elysiajs/cors';
 
 const port = Bun.env.PORT || 8080;
 
-new Elysia()
-  .ws('/ws', {
-    open: (ws) => {
-      console.log('Client connected');
-    },
+const todos = [
+  { id: 1, text: 'Buy milk' },
+  { id: 2, text: 'Buy eggs' },
+  { id: 3, text: 'Buy bread' },
+];
 
-    message: (ws, data) => {
-      console.log(data);
-      // ws.send('Hello from server!');
+const app = new Elysia()
+  .use(cors())
+  .ws('/ws', {
+    // open connection and send initial data
+    open(ws) {
+      // ws.subscribe('todos').send(todos);
+      ws.subscribe('todos').send({
+        type: 'todos',
+        payload: todos,
+      });
+    },
+    body: t.Object({
+      type: t.String(),
+      payload: t.Any(),
+    }),
+    // listen for messages
+    message(ws, msg) {
+      if (msg.type === 'createTodo') {
+        const newTodo = msg.payload;
+        todos.push({ ...newTodo, id: todos.length + 1 });
+
+        // so the server re-broadcasts incoming message to everyone
+        app.server!.publish(
+          'todos',
+          JSON.stringify({
+            type: 'todos',
+            payload: todos,
+          })
+        );
+      }
     },
   })
   .listen(port);
